ec26479c054eb326ac1da661497e13b029fc2bba
hiromu.saito <hiromu.saito@bigtreetc.com>
2021-10-24 20:38:52 +0900

    fix index.js

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable require-jsdoc */
 'use strict';
 
@@ -8,25 +9,64 @@ admin.initializeApp(functions.config().firebase);
 
 const fireStore = admin.firestore();
 exports.scheduledFuction = functions.pubsub
-    .schedule('every 24 hours').onRun((context) => {
+    .schedule('every 24 hours').onRun((context) =>{
       functions.logger.info('--------------function start -----------------');
       //  データの取得
       const userRef = fireStore.collection('users');
       const habitRef = fireStore.collection('habits');
-
+      const messageRef = fireStore.collection('message');
       userRef.get().then((userSnapShot)=>{
-        habitRef.get.then((habitSnapShot) => {
-          habitSnapShot.forEach((habit) => {
-            // トークンをuserSnapShotから取得
-            const user = userSnapShot.find((user)=>{
-              return user.uid = habit.uid;
+        habitRef.get().then((habitSnapShot) => {
+          messageRef.get().then((msgSnapShot) => {
+            let goodMsg;
+            let badMsg;
+            msgSnapShot.forEach((msgDoc) => {
+              const msg = msgDoc.data();
+              if (msg.id === 1) {
+                goodMsg = msg.text;
+              } else {
+                badMsg = msg.text;
+              }
             });
-            const token = user.token;
 
-            // 計算処理
-            // メッセージ取得
-            // 送信処理
-            pushToDevice(token, createMessage(habit.habitName));
+            habitSnapShot.forEach((habitDoc) => {
+              const habit = habitDoc.data();
+              let token;
+
+              userSnapShot.forEach((userDoc)=>{
+                const user = userDoc.data();
+                functions.logger.info(`user.uid:${user.uid}`);
+                if (user.uid === habit.uid) {
+                  token = user.token;
+                }
+              });
+              // 計算処理
+              const commits = habit.commits;
+              const oneWeekAgo = new Date();
+              oneWeekAgo.setDate(oneWeekAgo.getDate() - 6);
+
+              let commitCount = 0;
+              Object.keys(commits).forEach((key) => {
+                functions.logger.info(key);
+                const day = key.split('/').join('-') + 'T00:00:00';
+                const date = new Date(day);
+
+                const diff = getDateDiff(oneWeekAgo, date);
+                functions.logger.info(`day:${day}, diff:${diff}`);
+                if (diff >= 0) {
+                  commitCount++;
+                }
+                if (commitCount >= habit.frequency) {
+                  functions.logger.info(`${habit.habitName} is good! because frequency is ${habit.frequency} and commit is ${commitCount}.`);
+                  const body = createGoodMsgBody(goodMsg, habit.habitName);
+                  pushToDevice(token, createMessage(body));
+                } else {
+                  functions.logger.info(`${habit.habitName} is bad! because frequency is ${habit.frequency} but commit is ${commitCount}.`);
+                  const body = createBadMsgBody(badMsg, habit.habitName, habit.frequency);
+                  pushToDevice(token, createMessage(body));
+                }
+              });
+            });
           });
         });
       });
@@ -34,8 +74,7 @@ exports.scheduledFuction = functions.pubsub
 
 // eslint-disable-next-line require-jsdoc
 function pushToDevice(token, payload) {
-  functions.logger.info('pushToDevice:', token);
-
+  functions.logger.info(`pushToDevice:${token},message:${payload.notification.body}`);
   const options = {
     priority: 'high',
   };
@@ -49,14 +88,27 @@ function pushToDevice(token, payload) {
       });
 }
 
-function createMessage(habit) {
+function createMessage(body) {
   const payload = {
     notification: {
-      title: 'good!',
-      body: `${habit} is good!`,
+      title: 'Motivation Accelerator',
+      body: body,
       badge: '1',
       sound: 'default',
     },
   };
   return payload;
+}
+
+function getDateDiff(start, end) {
+  const time = end.getTime() - start.getTime();
+  return Math.floor(time / (1000 * 60 * 60 * 24));
+}
+
+function createGoodMsgBody(template, habitName) {
+  return template.replace('{habitName}', habitName);
+}
+
+function createBadMsgBody(template, habitName, frequency) {
+  return template.replace('{habitName}', habitName).replace('{frequency}', frequency);
 }
